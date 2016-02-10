aa5cfa0430f5298a08a7c2ad7e517b6b6eeac7ef
othman <benrahmaneothman@gmail.com>
2016-02-10 11:51:50 +0100

    avatar for user in logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,7 @@ def configure_permitted_parameters
 devise_parameter_sanitizer.for(:sign_up) << :username
 devise_parameter_sanitizer.for(:sign_up) << :email
 devise_parameter_sanitizer.for(:sign_up) << :avatar
+
 devise_parameter_sanitizer.for(:account_update) << :username
 devise_parameter_sanitizer.for(:account_update) << :email
 devise_parameter_sanitizer.for(:account_update) << :avatar
