48bb184df2a09389c8f5da7d35191fac1ebfbbc4
othman <benrahmaneothman@gmail.com>
2016-02-10 16:49:13 +0100

    un utilisateur ne peut effacer le post d'un autre

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -61,12 +61,16 @@ class PostsController < ApplicationController
   # DELETE /posts/1
   # DELETE /posts/1.json
   def destroy
-    @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+       if @post.user_id != current_user.id
+           redirect_to @post , notice: 'dont change other post'
+       else
+     @post.destroy
+     respond_to do |format|
+       format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+       format.json { head :no_content }
+         end
+     end
+   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
