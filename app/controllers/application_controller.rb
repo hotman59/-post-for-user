26562720b5ab3659c6bd6c7a062c45f444c93d96
othman <benrahmaneothman@gmail.com>
2016-02-10 10:03:08 +0100

    edit profil done application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,5 +10,7 @@ protected
 def configure_permitted_parameters
 devise_parameter_sanitizer.for(:sign_up) << :username
 devise_parameter_sanitizer.for(:sign_up) << :email
+devise_parameter_sanitizer.for(:account_update) << :username
+devise_parameter_sanitizer.for(:account_update) << :email
 end
 end
